4edf5ad16d823ff61994c409e7d1cad1ca5ece10
Apache <ed.stafford@gmail.com>
2017-12-29 16:40:00 +0000

    Removed extraneous DataTable statement

--- src/resources/views/request.blade.php
+++ src/resources/views/request.blade.php
@@ -163,14 +163,6 @@
     <script type="application/javascript">
         $(function () {
             $('#srps').DataTable()
-            $('#example2').DataTable({
-                'paging': true,
-                'lengthChange': false,
-                'searching': false,
-                'ordering': true,
-                'info': true,
-                'autoWidth': false
-            })
         })
     </script>
 
