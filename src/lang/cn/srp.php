bcf70ac7e0c8b6c82792c268a94c8ce1f6aa637d
JHXSMatthew <qilqil@live.com>
2019-03-11 16:36:02 +1100

    Chinese lang

---
+++ src/lang/cn/srp.php
@@ -0,0 +1,38 @@
+<?php
+
+return [
+    'shipType' => '船型',
+    'costs' => '花费',
+    'paidout' => '已打款',
+    'characterName' => '角色名',
+    'request' => '申请补损',
+    'id' => 'ID',
+    'submitted' => '待审核',
+    'changedby' => '最后修改',
+    'approvedby' => '审批人',
+
+    'list' => '申请列表',
+    'action' => '状态',
+    'metrics' => '统计',
+
+    'request_inst'      =>  '打开人物链接Character Sheet -> 交互 Interactions -> 战斗记录 Combat Log -> 击毁报告 Losses -> 外部链接 External URL。 黏贴到这个框里。',
+    'request_new'       =>  '申请补损',
+    'ping_info'         =>  '把集结信息写在这里。',
+    'ping'              =>  '集结信息',
+    'mysrp'             =>  '我的补损申请',
+    'verify_killmail'   =>  '读取击毁报告',
+    'submit_killmail'   =>  '提交',
+    'killmail_details'  =>  '击毁报告详情',
+    'pilot'             =>  '驾驶员',
+    'ship'              =>  '船',
+    'cost'              =>  '花费',
+
+    'low_slot_mod'      =>  '低槽',
+    'mid_slot_mod'      =>  '中槽',
+    'hi_slot_mod'       =>  '高槽',
+    'subsystems'        =>  '子系统',
+    'rigs'              =>  '船插',
+    'drone_bay'         =>  '无人机舱',
+    'cargo_bay'         =>  '货仓',
+    'number'            =>  '数量'
+];
