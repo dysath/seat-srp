b8954746e9ed9f4b28cfc37372abaeb3c9cadea7
Lena Schneider <darkgigabyte@googlemail.com>
2020-09-27 17:34:09 +0200

    bugfix for "stdclass property 'id' does not exist"

--- src/database/migrations/2020_03_01_231218_migrate_srp_version_four.php
+++ src/database/migrations/2020_03_01_231218_migrate_srp_version_four.php
@@ -26,7 +26,7 @@ class MigrateSrpVersionFour extends Migration
                 ->first();
             if (is_null($new_id) || is_null($new_id->new_user_id)){
                 DB::table('seat_srp_srp')
-                    ->where('id', $entry->id)
+                    ->where('kill_id', $entry->kill_id)
                     ->delete();
                 continue;
             }
