198bd3c2eb4bc62ba7723d17ce1898140bf0162c
FeiBam <feibam19690104@gmail.com>
2023-10-15 02:13:43 +0900

    use endpoint not domian

--- src/Helpers/SrpManager.php
+++ src/Helpers/SrpManager.php
@@ -285,8 +285,10 @@ trait SrpManager
 
         $partsList = implode("\n", $priceList);
 
+        $evepraisal = setting('denngarr_seat_srp_evepraisal_domain', true);
+
         $response = (new Client())
-            ->request('POST', 'http://evepraisal-absolute-order-com/appraisal.json?market=jita', [
+            ->request('POST', "$evepraisal/appraisal.json?market=jita", [
                 'multipart' => [
                     [
                         'name' => 'uploadappraisal',
